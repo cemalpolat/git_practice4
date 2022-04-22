b26d6fe07ae5324b6742a06bcf46b371038b8a23
Cemal Polat <cemalpolat0001@gmail.com>
2022-04-22 01:15:20 +0100

    Creating conflict in TestCase003 in master

---
+++ src/hello/TestCase003.java
@@ -0,0 +1,8 @@
+package hello;
+
+public class TestCase003 {
+    // this is unknown changes to feature branch
+    // taht exists in master
+    // now git will not know what to do with this
+    // when we merge and conflict will happen!
+}
